de9529ed137d02e63d8b3db3b6534a0359fa289b
PavanKumar <pavankumar117@yahoo.com>
2015-11-28 19:03:17 +0530

    Day12 Date picker

--- datepicker.js
+++ datepicker.js
@@ -8,55 +8,68 @@ var date = new Date(); //var date prints complete date
 var dd = date.getDate(); //var dd prints date i.e; 27 etc
 var d = date.getDay();
 var day = arday[d]; //var day prints day Friday
-var mon = date.getMonth(); //var month prints month november
+var mon = date.getMonth();
 var year = date.getFullYear(); //var year prints year 2015
-var month = armonth[mon];
+var month = armonth[mon];//var month prints month november
+console.log(d);
 
-function table(mon) {
+function table(x) {
   var x = mon;
-  console.log(x);
   document.getElementById('mytable').innerHTML='';
   var printtable;
   printtable += "<table border = '1'>"
   printtable += "<tr align = 'center'>"
-  printtable += "<td onclick = 'monthdec()'><</td>";
-  printtable += "<td>" + armonth[mon] + "</td>";
-  printtable += "<td onclick = 'monthinc()'>></td>";
+  printtable += "<td onclick = 'monthdec()' id = 'mondec'><</td>";
+  printtable += "<td  colspan = '5'>" + armonth[x] + "</td>";
+  printtable += "<td onclick = 'monthinc()' id = 'moninc'>></td>";
   printtable += "<td></td>"
-  printtable += "<td onclick = 'myfunction2()'><</td>"
-  printtable += "<td>" + year + "</td>";
-  printtable += "<td onclick = 'myfunction2()'>></td>"
   printtable += "</tr>"
   printtable += "<tr align = 'center'>";
   for (var i = 0; i < arday.length; i++) {
     printtable += "<td>" + arday[i] + "</td>";
   }
-  printtable += "</tr>"
+  printtable += "</tr>";
   printtable += "</table>";
+  $("#table1").html(printtable);
   document.getElementById('mytable').innerHTML= printtable;
 
-function monthdec() { //function for month decrement
-  console.log(x);
-  if (mon <= armonth.length) {
-    var temp = mon-1;
-    table(temp);
-  }
-}
-monthdec();
 }
 
+function monthdec() { //function for month decrement
+  if (mon <11 && mon >=1) {
+    mon = mon-1;
+    table(mon);
+  }
+}
 
 function monthinc() { //function for month incerement
-  if (mon <= armonth.length) {
-    var temp = mon+1;
-    table(temp);
+  if (mon < 11) {
+    mon = mon+1;
+    table(mon);
   }
 }
 
-function sayHello(name) {
-    var text = 'Hello ' + name;
-    var sayAlert = function() {
-      alert(text);
+function yeardec() {
+  year = year-1;
+  table(year);
+}
+
+function weekdays() {
+var weekday = new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
+console.log(weekday);
+date.setFullYear(date.getFullYear(),date.getMonth(),0);
+initial =0;
+start = d;
+for (var i = 0; i < 6 ; i++){
+  printtable1 += "<tr>"
+  for (var j = 0; i < 7; j++){
+    if (start = 0){
+      printtable1 += "<td></td>";
     }
-    sayAlert();
+    else{
+      break;
+    }
+  }
+}
+
 }
