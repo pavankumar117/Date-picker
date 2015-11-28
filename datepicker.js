a2a26c4af773946ff7ac0da10e381b7ca4fa0e6b
PavanKumar <pavankumar117@yahoo.com>
2015-11-28 15:47:57 +0530

    Date Picker

--- datepicker.js
+++ datepicker.js
@@ -4,108 +4,59 @@ var armonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
 var arday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
 'Saturday'];
 
+var date = new Date(); //var date prints complete date
+var dd = date.getDate(); //var dd prints date i.e; 27 etc
+var d = date.getDay();
+var day = arday[d]; //var day prints day Friday
+var mon = date.getMonth(); //var month prints month november
+var year = date.getFullYear(); //var year prints year 2015
+var month = armonth[mon];
 
-  var date = new Date();
-  var dd = date.getDate();
-  var d = date.getDay();
-  var day = arday[d];
-  var mon = date.getMonth();
-  var year = date.getFullYear();
-  var month = armonth[mon];
+function table(mon) {
+  var x = mon;
+  console.log(x);
+  document.getElementById('mytable').innerHTML='';
+  var printtable;
+  printtable += "<table border = '1'>"
+  printtable += "<tr align = 'center'>"
+  printtable += "<td onclick = 'monthdec()'><</td>";
+  printtable += "<td>" + armonth[mon] + "</td>";
+  printtable += "<td onclick = 'monthinc()'>></td>";
+  printtable += "<td></td>"
+  printtable += "<td onclick = 'myfunction2()'><</td>"
+  printtable += "<td>" + year + "</td>";
+  printtable += "<td onclick = 'myfunction2()'>></td>"
+  printtable += "</tr>"
+  printtable += "<tr align = 'center'>";
+  for (var i = 0; i < arday.length; i++) {
+    printtable += "<td>" + arday[i] + "</td>";
+  }
+  printtable += "</tr>"
+  printtable += "</table>";
+  document.getElementById('mytable').innerHTML= printtable;
 
-  //var dd prints date i.e; 27 etc
-  //var date prints complete date
-  //var day prints day Friday
-  //var month prints month november
-  //var year prints year 2015
-
-function table(a) {
-    document.getElementById('mytable').innerHTML='';
-    var str;
-    str += "<table border = '1'>"
-    str += "<tr align = 'center'>"
-    str += "<td onclick = 'mdec()'><</td>";
-    str += "<td id = 'getmonth'>" + armonth[a] + "</td>";
-    str += "<td onclick = 'minc()'>></td>";
-    str += "<td></td>"
-    str += "<td onclick = 'myfunction2()'><</td>"
-    str += "<td>" + year + "</td>";
-    str += "<td onclick = 'myfunction2()'>></td>"
-    str += "</tr>"
-    str += "<tr align = 'center'>";
-    for (var i=0; i<arday.length; i++) {
-      str += "<td>" + arday[i] + "</td>";
-    }
-    //str += "<td>Sun</td><td>Mon</td><td>Tue</td><td>wed</td><td>thur</td><td>Fri</td><td>Sat</td>"
-    str +="</tr>"
-    str += "</table>";
-    document.getElementById('mytable').innerHTML= str;
-    console.log ('hello');
+function monthdec() { //function for month decrement
+  console.log(x);
+  if (mon <= armonth.length) {
+    var temp = mon-1;
+    table(temp);
+  }
 }
-
-/*
-  function init() {
-    var name = "Mozilla"; // name is a local variable created by init
-    function displayName() { // displayName() is the inner function, a closure
-      alert(name); // use variable declared in the parent function
-    }
-    displayName();
-  };
-  init();
-*/
-
-
-
-
-
-
-
-function mdec() {
-    table();
-    console.log(c);
-    var c = mon-1;
-    if(c<armonth.length){
-    console.log(c);
-    table(c);
-  }
-  }
-
-
-function minc() {
-
-    var c = mon+1;
-    if(c<armonth.length){
-      table(c);
-    }
+monthdec();
 }
 
 
-
-
-
-
-
-
-/*function add(){
-  var a = 10;
-  console.log(a);
-  function sub(){
-    alert(a);
+function monthinc() { //function for month incerement
+  if (mon <= armonth.length) {
+    var temp = mon+1;
+    table(temp);
   }
-  return sub;
-};
-var all = add();
-all();
+}
 
-
-function makeFunc() {
-  var name = "Mozilla";
-  function displayName() {
-    alert(name);
-  }
-  return displayName;
-};
-
-var Func = makeFunc();
-Func();
-*/
+function sayHello(name) {
+    var text = 'Hello ' + name;
+    var sayAlert = function() {
+      alert(text);
+    }
+    sayAlert();
+}
