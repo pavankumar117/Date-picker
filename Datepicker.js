086cc6f0ca06abb57963fb899c80a180c2e3e9b8
PavanKumar <pavankumar117@yahoo.com>
2015-11-30 19:05:37 +0530

    Date picker

--- Datepicker.js
+++ Datepicker.js
@@ -2,90 +2,115 @@ var getdate  = new Date();
 var month = getdate.getMonth();
 var year = getdate.getFullYear();
 var Day = getdate.getDay();
-console.log(month);
 var no_of_month_days = new Date(getdate.getFullYear(),getdate.getMonth()+1,0).getDate();
-var armonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var armonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 var arweeks = ['Su', 'M', 'Tu', 'W', 'Th', 'F', 'Sa'];
-console.log(arweeks.length);
-console.log(armonths.length);
-	
-function printtables(temp,tempyear){
-	console.log(month);
+
+//******Function Date Picker********
+
+function Datepicker(temp,tempyear) {
 	var temp = month;
 	var printtable;
-	var tempyear = year; 
-	printtable += "<table border = '1' id = 'mytable'><tr align = 'center'><td onclick = 'monthinc()' class = 'click'><</td>";
-		printtable += "<td colspan = 3>" + armonths[temp] + "</td><td colspan = 2>" + tempyear + "</td>";
-		printtable += "<td onclick = 'monthdec()' class = 'click'>></td>";
-		printtable += "<tr align = 'center'>"
+	var tempyear = year;
+
+// Function for printing tables
+	function printtables(temp,tempyear) {
+		var start_day_of_month = 1; // defining variable =1 as months start with 1
+		var getweekday = new Date(tempyear,temp,1);//gets months start day
+		var weekday = getweekday.getDay();//gets where 1 day of month in a week starts with
+		var total_Days_in_month = new Date(tempyear,temp+1, 0).getDate();//gets total days in month
+		//var printtable;
+		var count = 0;
+		printtable += "<table id = 'mytable'>";
+		printtable += "<tr align = 'center'>";
+			printtable += "<th onclick = 'monthdec()' class = 'click'><</th>";
+			printtable += "<th colspan = '2'>" + armonths[temp] + "</th>";
+			printtable += "<th onclick = 'monthinc()' class = 'click'>></th>";
+			printtable += "<th onclick = 'yeardec()' class = 'click'><</th>";
+			printtable += "<th colspan = '2'>" + tempyear + "</th>";
+			printtable += "<th onclick = 'yearinc()' class = 'click'>></th>";
+		printtable += "</tr>";
+
+		printtable += "<tr align = 'center' colspan = '1'>"
 			for (var i = 0; i < arweeks.length; i++){
 				printtable += "<td>" + arweeks[i] + "</td>";
 			}
-		printtable += "</tr><tr>";
-		if (temp === 0 || temp === 2 || temp === 4 || temp === 6 || temp === 7 || temp === 9 || temp === 11 ) {
-			for( var i=1; i<=31; i++) {
-				printtable += "<td>" + i + "</td>";
-			}			
-		}
-		else if (temp === 3 || temp === 5 || temp === 8 || temp === 10 || temp === 12){
-			for( var i = 1; i <= 30; i++) {
-				printtable += "<td>" + i + "</td>";
-			}
-		}
 		printtable += "</tr>";
-		
-		printtable += "<tr>";
-		if (temp === 1) {
-			if (year % 100 === 0 ? year % 400 === 0 ? true : false : year % 4 === 0){
-				for( var i = 1; i <= 29; i++) {
-					printtable += "<td>" + i + "</td>";
+		//printing week days at their positions
+		printtable += "<tr align = 'center' colspan = '1'>";
+			for (var k = 0; k < arweeks.length; k++){
+				if (weekday === k) {
+					for(var i = 0; i < total_Days_in_month; i++) {
+						printtable += "<td>" + (i+1) + "</td>";
+						count++;
+						if(count % 7 === 0) {
+							printtable += "</tr><tr align = 'center' colspan = '1'>";
+						}
+					}
+				}
+				else {
+					printtable += "<td></td>";
+					count++;
 				}
 			}
-			else  {
-				for( var i = 1; i <= 28; i++) {
-				printtable += "<td>" + i + "</td>";
-				}
-			}
-		}
-	printtable += "</tr"; 
-	printtable += "</table>";
-		
-	$('#mytable').html(printtable);
-	document.getElementById('mydiv').innerHTML=printtable;	
-		
-}	
-	
-function monthinc() {
-	if(month < 0){
+			printtable += "</tr>";
+		printtable += "</table>";
+		document.getElementById('mydiv1').innerHTML=printtable;
+	}return printtables(temp,tempyear);
+
+	function getdate() {
+		$('#textbox').val(tempyear + ',' + temp+1);
+		$('#textbox').val(temp+1);
+	}
+	return getdate();
+}
+
+function monthdec() {    //function for decreasing the month and year
+	if (month <= 0) {
 		month = 11;
 		year--;
-		printtables(month,year);
+		Datepicker(month,year);
 	}
-	else{
+	else {
 		month = month-1;
-		printtables(month);
-	}
-}
-	
-function monthdec() {
-	if(month < armonths.length){
-		month = month+1;
-		printtables(month,year);
-	}
-	else{
-		month=0;
-		year++;
-		console.log(year);
-		printtables(month,year);
-		
+		Datepicker(month);
 	}
 }
 
-function check() {
-	if(year % 100 === 0 ? year % 400 === 0 ? true : false : year % 4 === 0) {
-		
+function monthinc() {   //function for increasing the month and year
+	if (month >= 11){
+		month=0;
+		year++;
+		Datepicker(month,year);
 	}
 	else {
-		console.log('not a leap year');
+		month = month+1;
+		Datepicker(month,year);
+	}
+}
+
+
+function yeardec() {    //function for decreasing the month and year
+	if (month <= 0) {
+		month = 11;
+		year--;
+		Datepicker(month,year);
+	}
+	else {
+		year = year-1;
+		Datepicker(month);
+	}
+}
+
+function yearinc() {   //function for increasing the month and year
+	console.log(year);
+	if (month >=11){
+		month=0;
+		year++;
+		Datepicker(month,year);
+	}
+	else {
+		year = year+1;
+		Datepicker(month,year);
 	}
 }
